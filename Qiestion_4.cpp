deab47ec2c60b786afbe4019a5867e887ef219bc
Ahad Ali <ranaahad9095@gmail.com>
2024-09-24 06:07:43 -0700

    Completing Question no. 4

--- Qiestion_4.cpp
+++ Qiestion_4.cpp
@@ -15,4 +15,56 @@ private:
     int rno;
     int marks[3];
     string name;
+public:
+    void input() {
+        cout << "Student Name : ";
+        getline(cin, name);
+        cout << "Student Roll no. : ";
+        cin >> rno;
+        cout << "\nStudent marks of 3 subjects\n";
+        for (int i = 0; i < 3; i++)
+        {
+            cout << "Marks[" << i << "] : ";
+            cin >> marks[i];
+        }
+        cin.ignore();
+    }
+    void show() const{
+        cout << "Student Name : " << name << endl;
+        cout << "Roll no. : " << rno << endl;
+        for (int i = 0; i < 3; i++)
+        {
+            cout << "Marks[" << i << "] : " << marks[i] << endl;
+        }
+    }
+    int total(){
+        int sum = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            sum = sum + marks[i];
+        }
+        return sum;
+    }
+    float avg(){
+        int sum = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            sum = sum + marks[i];
+        }
+        return sum/3.0;
+    }
 };
+
+int main()
+{
+    Result s1;
+    cout << "\n\tEnter Student Data\n\n";
+    s1.input();
+    cout << "-------------------------------\n";
+    s1.show();
+    cout << "-------------------------------\n";
+    cout << "Total Marks : " << s1.total() << endl;
+    cout << "Average Marks : " << s1.avg() << endl;
+    cout << "-------------------------------\n";
+    return 0;
+}
